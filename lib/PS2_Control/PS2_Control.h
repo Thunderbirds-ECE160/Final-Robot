1d982b098a4b75ec929a6f44c158af97c97e8f97
FaceFTW <westeraj@rose-hulman.edu>
2019-10-21 02:09:14 -0400

    PS2_Control has begun documentation
    -PS2_Control.h is fully completed in documentation
    -PS2_Control.cpp has file header, function comments are on TODO

--- lib/PS2_Control/PS2_Control.h
+++ lib/PS2_Control/PS2_Control.h
@@ -1,19 +1,39 @@
 /*************************************************************************************************************************************************
- * Project:
+ * File: PS2_Control.h
  * Author: Alex Westerman
- * Date Created:
+ * Date Created: 10/20/2019
  * Description
  * ======================
- *
- *
- * Important Functions
+ * This is a prototype file for the PS2_Control class, which defines Robot movement based on a PS2 Controller's input
+ * 
+ * Defined Values 
  * ======================
+ * (Associated values are indicated in parenthesis beside the defined name)
+ * DRIVE_FWD (1) - Value for the currentCMD associated with driving forward
+ * DRIVE_BACKWARD (2) - Value for the currentCMD associated with driving backward
+ * SPIN_LEFT (3) - Value for the currentCMD associated with spinning in place rotating counter-clockwise
+ * SPIN_RIGHT (4) - Value for the currentCMD associated with spinning in place rotating clockwise
+ * PIV_TRN_LEFT (5) - Value for the currentCMD associated with pivoting or turning toward the left
+ * PIV_TRN_RIGHT (6) - Value for the currentCMD associated with pivoting or turning toward the right
+ * GRAB (7) - Value for the currentCMD associated with activating the grabber
  *
- *
- * Misc Functions
+ * V_I_B_E_S (0) - A required value for working with the PS2 Controller related to controller vibration strength
+ * 
+ * 
+ * Class Variables
  * ======================
- *
- *
+ * private PS2X* ps2_Gamepad - Pointer to a PS2 Controller object, used for reading input from a PS2 Controller
+ * private Drive* robot - Pointer to a Drive object, used for controlling robot movement 
+ * private int currentCMD - Integer used as a storage for the parsed input from the PS2 Controller
+ * private boolean isTurnMode - Boolean serving as a toggle between pivoting and turning
+ * 
+ * Class Functions
+ * ======================
+ * public PS2_Control(PS2X*, Drive*) - Object Constructor
+ * public ~PS2_Control() - Object Destructor (NOT USED)
+ * public void init() - Initializaition function used to properly activate the PS2 Controller and Drive objects
+ * public void read_controller() - Function that reads and converts input from the PS2 Controller to an int stored as currentCMD. Calls parseCMD() after execution
+ * public void parseCMD() - Executes the proper movement based on the value of currentCMD 
  **************************************************************************************************************************************************/
 #include <PS2X_lib.h>
 #include <Drive.h>
@@ -48,7 +68,7 @@ class PS2_Control {
 PS2_Control::PS2_Control(PS2X gamepad, Drive robo) {
         ps2_Gamepad =&gamepad;
         robot = &robo;
-\
+
 }
 
 PS2_Control::~PS2_Control() {}
